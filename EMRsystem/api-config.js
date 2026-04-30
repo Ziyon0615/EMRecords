dd1e0bcb61a30eb956e45a19daf3f4efea52ec6e
Milo2024-01 <laurapresley4@gmail.com>
2026-05-01 01:14:35 +0800

    Point frontend to Render backend

--- EMRsystem/api-config.js
+++ EMRsystem/api-config.js
@@ -1,5 +1,5 @@
 (function () {
-  const renderApiUrl = 'https://emrsystem-api.onrender.com';
+  const renderApiUrl = 'https://emrsystem-9gng.onrender.com';
   const isLocalhost = ['localhost', '127.0.0.1'].includes(window.location.hostname);
   const apiBaseUrl = isLocalhost ? 'http://localhost:3000' : renderApiUrl.replace(/\/$/, '');
 
